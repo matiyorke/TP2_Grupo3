54f5205c01a6a79c77f15df4a283d86d8b07effb
Tomas Torregiani <tomitorre91@gmail.com>
2026-09-09 16:15:21 -0300

    implemente herencia en ej2

--- src/Ejercicio2/EdificioDeOficinas.java
+++ src/Ejercicio2/EdificioDeOficinas.java
@@ -1,5 +1,11 @@
 package Ejercicio2;
 
-public class EdificioDeOficinas {
+public class EdificioDeOficinas implements Edificio{
+
+	@Override
+	public double getSuperficieEdificio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- src/Ejercicio2/PoliDeportivo.java
+++ src/Ejercicio2/PoliDeportivo.java
@@ -1,5 +1,17 @@
 package Ejercicio2;
 
-public class PoliDeportivo {
+public class PoliDeportivo implements InstalacionDeportiva, Edificio {
+
+	@Override
+	public double getSuperficieEdificio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTipoDeInstalacion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
